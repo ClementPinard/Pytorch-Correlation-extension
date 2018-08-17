71c763bf7332f0ffce8790ed39367896c39f9df6
Clément Pinard <clement.pinard@parrot.com>
2018-08-17 15:45:13 +0200

    increment version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='spatial_correlation_sampler',
-    version="0.0.3",
+    version="0.0.4",
     author="Clément Pinard",
     author_email="clement.pinard@ensta-paristech.fr",
     description="Correlation module for pytorch",
